68610e195d9cf06977326289be149744402b5352
mathieu stumpf guntz <mathieu.stumpf@netapsys.fr>
2016-07-08 23:23:46 +0200

    Taking command line arguments as list of category + some pep8 cleanup

--- score.py
+++ score.py
@@ -2,29 +2,40 @@
 # -*- coding: utf-8 -*-
 
 import wikipedia
-import operator # for dummy sorting
+import operator  # for dummy sorting
+import sys
 
 sample = 'Philosophers'
 
 # help(cat_page)
 
+
 def getPagesIn(category):
-	"""Should return the list of pages that the provided category contains"""
-	page_name = 'Category:' + category
-	page = wikipedia.page(page_name)
-	return page.links
+    """Should return the list of pages that the provided category contains"""
+    page_name = 'Category:' + category
+    page = wikipedia.page(page_name)
+    return page.links
+
 
 def readability_score(page):
     summary = wikipedia.summary(page)
-    return 100./(len(summary)+1) # the shorter the better, but avoid zero
+    return 100./(len(summary)+1)  # the shorter the better, but avoid zero
 
-pages = getPagesIn(sample)
-scores = dict()
-for page in pages:
-    scores[page] = readability_score(page)
 
-sorted_scores = sorted(scores.items(), key=operator.itemgetter(1))
-for page, score in sorted_scores:
-    print page, score
-"""
-    """
+def get_scored_table(category):
+    print category
+    pages = getPagesIn(category)
+    scores = dict()
+    for page in pages:
+        scores[page] = readability_score(page)
+
+    sorted_scores = sorted(scores.items(), key=operator.itemgetter(1))
+    for page, score in sorted_scores:
+        print page, score
+
+if __name__ == "__main__":
+    categories = sys.argv
+    del categories[0]
+    for category in categories:
+        get_scored_table(category)
+        print '---'
