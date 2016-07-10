f089e5fe39d55d1046ae7f6d01f716fc74e1f720
mathieu stumpf guntz <mathieu.stumpf@netapsys.fr>
2016-07-10 11:03:17 +0200

    Add a quick and dirty HTML interface to the php version

--- catscore.php
+++ catscore.php
@@ -58,7 +58,8 @@ function fetchSummaries($pages){
     $untreated = count($pages);
     $summaries = array();
     for($i = 0; $i < count($pages); $i += $exlimit){
-        //var_dump($pages);
+		// extract summuries by slices according to the limitation on how many
+        // for entry can be fetched at a time by bots
         $page_buffer = array_slice ( $pages, $i, $exlimit);
         $titles = concatTitles($page_buffer);
         //echo $titles;
@@ -67,16 +68,41 @@ function fetchSummaries($pages){
     return $summaries;
 }
 
-$category = "Philosophy";
-$pages = categoryMembers($category);
-$summaries = fetchSummaries($pages);
+function getScores($category){
+	$pages = categoryMembers($category);
+	$summaries = fetchSummaries($pages);
 
-//var_dump( $summaries);
-$scores = array();
-foreach($summaries as $page){
-    $scores[$page->title] = score($page->extract);
+	//var_dump( $summaries);
+	$scores = array();
+	foreach($summaries as $page){
+		$scores[$page->title] = score($page->extract);
+	}
+	asort($scores);
+	return $scores;
 }
-asort($scores);
-var_dump( $scores);
-/*
- */
+
+// Section for flow control
+if(!isset($_GET["cat"]))
+	$category = 'Philosophy'; // User didn't ask anything, but hey…
+else
+	$category = $_GET["cat"]; // User parameter 
+$category = filter_var($category, FILTER_SANITIZE_SPECIAL_CHARS); // avoid  '<script>alert("evil code here!")</script>'
+// $category = escapeshellarg($category); // Should be safe like that
+
+$scores = getScores($category);
+//var_dump($scores);
+// Section for HTML output formating
+?>
+<html>
+<body>
+<h1>Summary scoring for some articles in the <?php echo $category ?> category</h1>
+<table>
+<tr><th>Title</th><th>Score</th>
+<?php
+foreach($scores as $title => $value)
+        echo '<tr><td>' . $title . '</td><td>' . $value . "</td></tr>\n";
+?>
+</table>
+</body>
+</html>
+
